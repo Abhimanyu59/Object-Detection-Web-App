fc9aac32054912a0acbfcccecf8d76f424f3f4f9
Abhimanyu Singh <74282456+Abhimanyu59@users.noreply.github.com>
2022-07-25 18:48:19 +0530

    Add files via upload

---
+++ Smart_Band.js
@@ -0,0 +1,34 @@
+img = "";
+status = "";
+
+
+function back(){
+    window.location = "index.html";
+}
+
+function preload(){
+    img = loadImage('Smart_Band.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("Text_color").innerHTML = "Detecting Objects";
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+
+function modelLoaded(){
+    status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
